b402a94449099a3f00896b601fec790ead77e453
Kosyrev143 <artemkosyrev93@gmail.com>
2026-02-05 12:11:09 +0500

    feat: (pz17)

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace App1
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Button button3 = new Button();
+            button3.Content = "Кнопка 3";
+            button3.Width = 120;
+            button3.Height = 40;
+            button3.Margin = new Thickness(200, 0, 0, 0);
+            button3.Background = new SolidColorBrush(Windows.UI.Colors.White);
+            button3.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+            button3.FontFamily = new FontFamily("Verdana");
+            layout.Children.Add(button3);
+        }
+
+        private void buttonClick(object sender, RoutedEventArgs e)
+        {
+            button1.Content = "успешно";
+        }
+    }
+}
---
+++ App1/App1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+
+        public override string ToString()
+        {
+            return $"Student {this.Name} from Group {this.Group}";
+        }
+    }
+}
