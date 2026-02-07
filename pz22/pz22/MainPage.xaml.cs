731c67b083d8e74eef4ded673638a03014f1db79
Kosyrev143 <artemkosyrev93@gmail.com>
2026-02-07 12:47:02 +0500

    feat :(pz22)

---
+++ pz22/pz22/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace pz22
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateTotal(object sender, RoutedEventArgs e) {
+            int total = 0;
+
+            if (chkEspresso.IsChecked == true) total += 150;
+            if (chkCappuccino.IsChecked == true) total += 200;
+            if (chkLatte.IsChecked == true) total += 220;
+
+            if (chkCroisant.IsChecked == true) total += 120;
+            if (chkBrownie.IsChecked == true) total += 180;
+
+            if ((rbMedium != null) && rbMedium.IsChecked == true) total += 50;
+            if ((rbLarge != null) && rbLarge.IsChecked == true) total += 100;
+
+            if(txtTotal != null) txtTotal.Text = $"{total} р";
+
+
+        }
+    }
+}
